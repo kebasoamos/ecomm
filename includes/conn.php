fcac25ab4ae82702b9f77fbb8f1db722b03f8be9
kebasoamos <67126424+kebasoamos@users.noreply.github.com>
2021-10-25 17:26:25 +0300

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -2,9 +2,9 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=ecomm";
-	private $username = "root";
-	private $password = "";
+	private $server = "mysql:host=us-cdbr-east-04.cleardb.com;dbname=heroku_81e8c5c4b568026";
+	private $username = "b6ae1537b3494c";
+	private $password = "daf1984f";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  	
